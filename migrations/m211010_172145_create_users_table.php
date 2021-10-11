705c905c150121d2ab1eb9d4482fb65cd98bd95e
webmans-ru <lukoyanov_aa@webmens.ru>
2021-10-11 13:33:38 +0300

    +

--- migrations/m211010_172145_create_users_table.php
+++ migrations/m211010_172145_create_users_table.php
@@ -5,15 +5,23 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%test}}`.
  */
-class m211010_172145_create_test_table extends Migration
+class m211010_172145_create_users_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->createTable('{{%test}}', [
+        $this->createTable('{{%users}}', [
             'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'password' => $this->string()->notNull(),
+            'b24_user_id' => $this->integer(),
+            'name' => $this->string(),
+            'last_name' => $this->string(),
+            'access_token' => $this->string(),
+            'date_expired' => $this->string(),
+            'auth_key' => $this->string(),
         ]);
     }
 
@@ -22,6 +30,6 @@ class m211010_172145_create_test_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('{{%test}}');
+        $this->dropTable('{{%users}}');
     }
 }
