71261d940975376445901cd92cc4b07dc9769a99
lukoyanov-aa <lukoyanov_aa@webmens.ru>
2021-12-25 10:42:57 +0300

    Обновлён функционал placementController

--- controllers/handlers/placements/PlacementBaseController.php
+++ controllers/handlers/placements/PlacementBaseController.php
@@ -9,18 +9,16 @@ use yii\web\Controller;
 use Bitrix24\User\User as B24User;
 use wm\admin\models\User;
 
-class PlacementBaseController extends Controller {
-    
+class PlacementBaseController extends Controller
+{
     protected $accessToken = null;
-
     public $layout = '@app/views/layouts/app.php';
 
-    public function beforeAction($action) {
+    public function beforeAction($action)
+    {
         $this->enableCsrfValidation = false;
-
         $request = Yii::$app->request;
         $session = Yii::$app->session;
-
         if ($request->get('DOMAIN') && $request->post('member_id') && $request->post('AUTH_ID') && $request->post('REFRESH_ID')) {
             $component24 = new \wm\b24tools\b24Tools();
             $arAccessParams = $component24->prepareFromRequest(Yii::$app->request->post(), Yii::$app->request->get());
@@ -28,7 +26,6 @@ class PlacementBaseController extends Controller {
             if ($errors) {
                 throw new HttpException(403, 'В доступе отказано');
             }
-            
             $b24App = $component24->connectFromUser($arAccessParams);
             $obB24 = new B24User($b24App);
             $b24User = $obB24->current()['result'];
@@ -38,28 +35,21 @@ class PlacementBaseController extends Controller {
                 $user = new User();
                 $user->username = ArrayHelper::getValue($b24User, 'EMAIL');
                 $user->b24_user_id = ArrayHelper::getValue($b24User, 'ID');
-
                 $user->name = ArrayHelper::getValue($b24User, 'NAME');
                 $user->last_name = ArrayHelper::getValue($b24User, 'LAST_NAME');
-
                 $user->password = \Yii::$app->security->generatePasswordHash($userPassword);
-                $user->getAccessToken();
-                $user->save();
-            }else{
-                $user->getAccessToken();
-                $user->save();
             }
+
+            $user->getAccessToken();
+            $user->b24AccessParams = json_encode($arAccessParams);
+            $user->save();
             $this->accessToken = $user->access_token;
-            
-            Yii::$app->user->login($user, 3600*24*30);
-            
+            Yii::$app->user->login($user, 3600 * 24 * 30);
             $session->set('accessAllowed', true);
             $session['AccessParams'] = $arAccessParams;
-            
-            
         }
         if (null === $request->get('DOMAIN') or null === $request->post('member_id') or null === $request->post('AUTH_ID') or null === $request->post('REFRESH_ID')) {
-            throw new HttpException(404, 'Приложение необходимо запустить из портала Битрикс24');
+            throw new HttpException(403, 'Приложение необходимо запустить из портала Битрикс24');
         }
         return parent::beforeAction($action);
     }
