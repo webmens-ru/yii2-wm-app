1dabfcaf6b3220af26c851d0bede1c5453a2c266
lukoyanov-aa <lukoyanov_aa@webmens.ru>
2022-01-19 15:49:05 +0300

    Добавлено сохранение адреса сайта в JS переменную

--- views/app/index.php
+++ views/app/index.php
@@ -1,5 +1,7 @@
 <?php
 use app\assets\AppAsset;
+use yii\helpers\Url;
+
 $assetsUrl = AppAsset::register($this);
 $this->registerJsFile($assetsUrl->baseUrl . '/static/js/runtime-main.js');
 $this->registerJsFile($assetsUrl->baseUrl . '/static/js/2.chunk.js');
@@ -11,5 +13,6 @@ $this->registerJsFile($assetsUrl->baseUrl . '/static/js/main.chunk.js');
     window._ACCESS_TOKEN_ = '<?=$accessToken?>';
     window._PARAMS_= <?=$params?>;
     window._APP_URL_='<?=$appUrl?>';
+    window._HOSTNAME_= "<?=Url::base('https')?>"
 </script>
 <script src="//api.bitrix24.com/api/v1/"></script>
